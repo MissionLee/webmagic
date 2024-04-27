77e5f1d206ae3f80927ae2e0f772b3d1d7e39e56
Mission Li <zuixue3000@163.com>
2024-04-27 15:09:18 +0800

    1

--- missionlee/src/main/java/pers/missionlee/chan/starter/SpiderStarter.java
+++ missionlee/src/main/java/pers/missionlee/chan/starter/SpiderStarter.java
@@ -436,7 +436,6 @@ public class SpiderStarter {
                             diskService.checkBookArtistPath(dataBaseService);
                         } else if (4 == mode) {
                             diskService.mergePicVid();
-                            System.out.println("8888888888888888888888 end 888888888888888888888");
                         } else if (5 == mode) {
                             diskService.findPathError();
                         } else if (6 == mode) {
@@ -1070,6 +1069,4 @@ public class SpiderStarter {
             }
         }
     }
-
-
 }
